9df4e71230061a4a345c26e22823ce3373d6a58b
Fernando Freire <fernando.freire@nike.com>
2017-09-14 09:07:53 -0700

    Add simple test for flow module

--- tests/test_signal_analog.py
+++ tests/test_signal_analog.py
@@ -3,34 +3,9 @@
 
 """Tests for `signal_analog` package."""
 
-import pytest
-
-from signal_analog import signal_analog
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+from signal_analog import flow
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-class Test_Signal_analog():
-    """Tests for `signal_analog` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
+def test_data_example():
+    """Ensure that the data function can be correctly serialized"""
+    assert str(flow.Data('cpu.utilization')) == 'data("cpu.utilization")'
