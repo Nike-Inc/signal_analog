9388b4a918137c0960e497483219f6df3fe17b72
Fernando Freire <fernando.freire@nike.com>
2017-10-02 22:38:35 -0700

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ test_requirements = [
 
 setup(
     name='signal_analog',
-    version='0.3.1',
+    version='0.3.2',
     description="Troposphere-like library for building and composing SignalFx SignalFlow programs.",
     long_description=readme + '\n\n' + history,
     author="Fernando Freire",
