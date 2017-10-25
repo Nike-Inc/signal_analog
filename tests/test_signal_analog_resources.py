eb4682b38eef58f9e1893a6c690dc3c1b33ff90d
Fernando Freire <fernando.freire@nike.com>
2017-10-25 10:16:01 -0700

    Add remaining chart option builders
    Also did some refactoring to move is_valid and enum checks into a util
    module. This way they can be shared with objects that don't inherit from
    Resource.
    
    Some more work will need to be done to make sure that docs are updated
    for all objects. The intention is that users can make use of this
    library without having prior knowledge of the API, so to the best of our
    ability we should make sure the docs are clear.

--- tests/test_signal_analog_resources.py
+++ tests/test_signal_analog_resources.py
@@ -3,6 +3,7 @@ import requests_mock
 from requests.exceptions import HTTPError
 
 from signal_analog.resources import Resource, __SIGNALFX_API_ENDPOINT__
+import signal_analog.util as util
 import json
 
 bad_str_inputs = [None, ""]
@@ -10,103 +11,103 @@ bad_str_inputs = [None, ""]
 
 @pytest.mark.parametrize("expected", bad_str_inputs)
 def test_resource_init_empty_opts_args(expected):
-	with pytest.raises(ValueError):
-		Resource(base_url=expected)
-		Resource(endpoint=expected)
-		Resource(api_token=expected)
-		Resource().with_api_token(expected)
+        with pytest.raises(ValueError):
+                Resource(base_url=expected)
+                Resource(endpoint=expected)
+                Resource(api_token=expected)
+                Resource().with_api_token(expected)
 
 
 @pytest.mark.parametrize("input,expected",
                          [(None, __SIGNALFX_API_ENDPOINT__), ("foo", "foo")])
 def test_resource_init_base_url(input, expected):
-	if input is None:
-		resource = Resource()
-	else:
-		resource = Resource(input)
+        if input is None:
+                resource = Resource()
+        else:
+                resource = Resource(input)
 
-	assert resource.base_url == expected
+        assert resource.base_url == expected
 
 
 @pytest.mark.parametrize("input,expected", [(None, '/'), ('/foo', '/foo')])
 def test_resource_init_endpoint(input, expected):
-	# TODO would be interesting to validate endpoints always start with '/'
-	if input is None:
-		resource = Resource()
-	else:
-		resource = Resource(endpoint=input)
+        # TODO would be interesting to validate endpoints always start with '/'
+        if input is None:
+                resource = Resource()
+        else:
+                resource = Resource(endpoint=input)
 
-	assert resource.endpoint == expected
+        assert resource.endpoint == expected
 
 
 def test_resource_init_api_token():
-	expected = "foo"
-	resource = Resource(api_token=expected)
-	assert resource.api_token == expected
+        expected = "foo"
+        resource = Resource(api_token=expected)
+        assert resource.api_token == expected
 
 
 def test_is_valid_without_message():
-	with pytest.raises(ValueError) as error:
-		Resource().is_valid(None, error_message=None)
-		assert error.message is None
+        with pytest.raises(ValueError) as error:
+                util.is_valid(None, error_message=None)
+                assert error.message is None
 
 
 def test_resource_with_api_token():
-	expected = 'foo'
-	resource = Resource().with_api_token(expected)
+        expected = 'foo'
+        resource = Resource().with_api_token(expected)
 
-	assert resource.api_token == expected
+        assert resource.api_token == expected
 
 
 @pytest.mark.parametrize("api_token,options", [(None, {}), ("foo", {})])
 def test_resource_create_failing_preconds(api_token, options):
-	with pytest.raises(ValueError):
-		resource = Resource()
-		resource.api_token = api_token
-		resource.options = options
+        with pytest.raises(ValueError):
+                resource = Resource()
+                resource.api_token = api_token
+                resource.options = options
 
-		resource.create()
+                resource.create()
 
 
 def test_resource_create_happy_path():
-	with requests_mock.Mocker() as mock:
-		expected = {'foo': 'bar'}
+        with requests_mock.Mocker() as mock:
+                expected = {'foo': 'bar'}
 
-		mock.register_uri(
-			'POST', __SIGNALFX_API_ENDPOINT__ + '/', json=expected
-		)
+                mock.register_uri(
+                        'POST', __SIGNALFX_API_ENDPOINT__ + '/', json=expected
+                )
 
-		resource = Resource().with_api_token('test')
-		resource.options = {'opt': 'val'}
+                resource = Resource().with_api_token('test')
+                resource.options = {'opt': 'val'}
 
-		response = resource.create()
-		assert response == expected
+                response = resource.create()
+                assert response == expected
 
 
 def test_resource_create_error():
-	with requests_mock.Mocker() as mock:
-		mock.register_uri(
-			'POST', __SIGNALFX_API_ENDPOINT__ + '/', status_code=500
-		)
+        with requests_mock.Mocker() as mock:
+                mock.register_uri(
+                        'POST', __SIGNALFX_API_ENDPOINT__ + '/', status_code=500
+                )
 
-		resource = Resource().with_api_token('test')
-		resource.options = {'opt': 'val'}
+                resource = Resource().with_api_token('test')
+                resource.options = {'opt': 'val'}
 
-		with pytest.raises(HTTPError):
-			resource.create()
+                with pytest.raises(HTTPError):
+                        resource.create()
 
 
 def test_resource_create_dry_run():
-	with requests_mock.Mocker() as mock:
-		expected = json.dumps({'opt': 'val'})
+        with requests_mock.Mocker() as mock:
+                expected = json.dumps({'opt': 'val'})
 
-		mock.register_uri(
-			'POST', __SIGNALFX_API_ENDPOINT__ + '/', json=expected
-		)
+                mock.register_uri(
+                        'POST', __SIGNALFX_API_ENDPOINT__ + '/', json=expected
+                )
 
-		resource = Resource().with_api_token('test')
-		resource.options = {'opt': 'val'}
+                resource = Resource().with_api_token('test')
+                resource.options = {'opt': 'val'}
 
-		response = resource.create(dry_run=True)
+                response = resource.create(dry_run=True)
 
-		assert response == expected
+                assert response == expected
