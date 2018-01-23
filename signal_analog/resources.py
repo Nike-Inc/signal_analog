5dc75a30cd433be369f2b5db5bd928311495fc2c
Fernando Freire <fernando.freire@nike.com>
2018-01-22 20:21:01 -0800

    Refactor resource create method to general purpose internal helper
    Now we need to refactor the chart/dashboard resources to take advantage
    of this.

--- signal_analog/resources.py
+++ signal_analog/resources.py
@@ -8,7 +8,7 @@ __SIGNALFX_API_ENDPOINT__ = 'https://api.signalfx.com/v2'
 class Resource(object):
 
     def __init__(self, base_url=__SIGNALFX_API_ENDPOINT__, endpoint='/',
-                 api_token=None):
+                 api_token=None, session=None):
         """Encapsulation for resources that can exist in the SignalFx API.
 
         This version of the Resource class does not manage any state with the
@@ -21,6 +21,8 @@ class Resource(object):
             base_url: the base endpoint to use when talking to SignalFx
             endpoint: the particular endpoint to hit for this resource
             api_token: the api token to authenticate requests with
+            session: optional session harness for making API requests. Mostly
+                     used in test scenarios.
         """
 
         # Users may want to provide this via the `with_*` builder instead of
@@ -28,6 +30,11 @@ class Resource(object):
         # don't pass one in at this point.
         if api_token is not None:
             self.__set_api_token__(api_token)
+
+        if session:
+            self.session_handler = session
+        else:
+            self.session_handler = requests.Session()
 
         self.__set_endpoint__(endpoint)
         self.__set_base_url__(base_url)
@@ -58,6 +65,56 @@ class Resource(object):
         self.__set_api_token__(token)
         return self
 
+    def __stateful_action__(self, action, endpoint, update_fn,
+                    params=None, dry_run=False, interactive=False, force=False):
+        """Perform a stateful HTTP action against the SignalFx API.
+
+        Arguments:
+            action: the stateful action to take
+            update_fn: callback allowing modification of the payload before
+                       sending to SignalFx.
+            dry_run: When true, this resource prints its configured state
+                     without calling SignalFx.
+            interactive: When true, this resource asks the caller which version
+                         resource to modify.
+            force: When true, this resource modifies itself in SignalFx
+                   disregarding previous SignalFx state.
+
+            Returns:
+                The JSON response if successful.
+
+            Raises:
+                HTTPError: when a network exception ocurred
+        """
+        util.is_valid(self.options)
+
+        if dry_run:
+            return self.options
+
+        if not action or action.lower() not in ['post', 'put']:
+            msg = '{0} is not a supported stateful action.'
+            raise ValueError(msg.format(action))
+
+        util.is_valid(self.api_token)
+
+        response = self.session_handler.request(
+            action,
+            url=self.base_url + endpoint,
+            params=params,
+            json=update_fn(self.options),
+            headers={
+                'X-SF-Token': self.api_token,
+                'Content-Type': 'application/json'
+            })
+
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as error:
+            # Tell the user exactly what went wrong according to SignalFX
+            raise RuntimeError(error.response.text)
+
+        return response.json()
+
     def create(self, dry_run=False):
         """Create this resource in the SignalFx API.
 
@@ -73,29 +130,6 @@ class Resource(object):
             missing.
         """
 
-        util.is_valid(self.options)
-
-        if dry_run is False:
-            # TODO figure out better abstraction for validating pre_conditions
-            util.is_valid(self.api_token)
-
-            response = requests.post(
-                url=self.base_url + self.endpoint,
-                data=json.dumps(self.options),
-                headers={
-                    'X-SF-Token': self.api_token,
-                    'Content-Type': 'application/json'
-                }
-            )
-
-            try:
-                # Throw an exception if we received a non-2xx response.
-                response.raise_for_status()
-            except requests.exceptions.HTTPError as error:
-                # Tell the user exactly what went wrong according to SignalFx
-                raise RuntimeError(error.response.text)
-
-            # Otherwise, return our status code
-            return response.json()
-        else:
-            return json.dumps(self.options)
+        id = lambda x: x
+        return self.__stateful_action__('post', self.endpoint, id,
+            dry_run=dry_run)
