5026c384b2b4a4723ed31bbd7dfc9558c1e549e0
Fernando Freire <fernando.freire@nike.com>
2018-02-23 15:03:23 -0800

    Add simple request logging

--- signal_analog/__init__.py
+++ signal_analog/__init__.py
@@ -2,6 +2,27 @@
 
 """Top-level package for Signal Analog."""
 
+import logging.config
+import pkg_resources
+import yaml
+
+# These two lines disable debugging at httplib level
+# (requests->urllib3->http.client)
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 0
+
+
 __author__ = """Fernando Freire"""
 __email__ = '***REMOVED***'
 __version__ = '0.20.0'
+
+logging_config = pkg_resources.resource_string(
+    __name__, 'logging.yaml').decode('utf-8')
+
+logging.config.dictConfig(yaml.load(logging_config))
+
+logger = logging.getLogger()
