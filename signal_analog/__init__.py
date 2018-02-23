6db5ac903bbf66de332c769b162996d763fbdd1a
Fernando Freire <fernando.freire@nike.com>
2018-02-23 15:06:50 -0800

    Remove extraneous urllib config and log to stdout
    No need to persist to file.

--- signal_analog/__init__.py
+++ signal_analog/__init__.py
@@ -6,16 +6,6 @@ import logging.config
 import pkg_resources
 import yaml
 
-# These two lines disable debugging at httplib level
-# (requests->urllib3->http.client)
-try:
-    import http.client as http_client
-except ImportError:
-    # Python 2
-    import httplib as http_client
-http_client.HTTPConnection.debuglevel = 0
-
-
 __author__ = """Fernando Freire"""
 __email__ = 'Lst-nike.plus.platform.sharedinfrastructure@nike.com'
 __version__ = '0.20.0'
