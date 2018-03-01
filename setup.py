a9342646abd0d58af88a55a83bed8ce0c48bac87
Jeremy Hicks <jeremy.hicks@nike.com>
2018-03-01 12:24:03 -0800

    Bump version: 0.21.0 → 0.22.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='signal_analog',
-    version='0.21.0',
+    version='0.22.0',
     description="Troposphere-like library for building and composing SignalFx SignalFlow programs.",
     long_description=readme + '\n\n' + history,
     author="Fernando Freire",
