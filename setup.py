bfda954eeb41876d837cbc639e67aade7236e74c
Todd Lisonbee <todd.lisonbee@nike.com>
2019-03-26 11:10:14 -0700

    Bump version: 2.5.0 → 2.6.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ test_requirements = [
 
 setup(
     name='signal_analog',
-    version='2.5.1',
+    version='2.6.0',
     description='A troposphere-like library for managing SignalFx'
                 + 'Charts, Dashboards, and Detectors.',
     long_description=readme + '\n\n' + history,
